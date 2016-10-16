dfe7f15c4f8537d71c3d2a8a7e115a47b37325c1
root <root@localhost.localdomain>
2016-10-16 03:36:51 -0500

    Fix argument order

--- student-distrib/interrupt.h
+++ student-distrib/interrupt.h
@@ -5,7 +5,7 @@
 
 void interrupt_handler(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx,
 	uint32_t esi, uint32_t edi, uint32_t esp, uint32_t ebp,
-	uint32_t error, uint32_t int_num,
+	uint32_t int_num, uint32_t error,
 	uint32_t eip, uint32_t cs, uint32_t eflags);
 
 extern void interrupt_handler_0(void);
