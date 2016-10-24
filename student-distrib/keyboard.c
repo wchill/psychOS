3cbde2b14a501aa63fba14649979e4f48cd2ffa4
Rodney <shaghou2@illinois.edu>
2016-10-23 19:51:03 -0500

    fixed Caps and shift

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -83,6 +83,86 @@ static const uint8_t keyboard_map_shift[KEYBOARD_SIZE] = {
     0,          // Nonstandard keys
 };
 
+static const uint8_t keyboard_map_caps[KEYBOARD_SIZE] = {
+    0,          // 00 - Error code
+    27,         // 01 - Esc
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
+    '\b',       // 0e - Backspace
+    '\t',       // 0f - Tab
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']',
+    '\n',       // 1c - Enter
+    0,          // 1d - Left Control
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', '`',
+    0,          // 2a - Left Shift
+    '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/',
+    0,          // 36 - Right Shift
+    '*',        // 37 - Print Screen / Keypad *
+    0,          // 38 - Left Alt
+    ' ',        // 39 - Space
+    0,          // 3a - Caps Lock
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 3b to 44 - F1 to F10
+    0,          // 45 - Num Lock
+    0,          // 46 - Scroll Lock
+    0,          // 47 - Home / Keypad 7
+    0,          // 48 - Up / Keypad 8
+    0,          // 49 - Page Up / Keypad 9
+    '-',        // 4a - Keypad "-"
+    0,          // 4b - Left / Keypad 4
+    0,          // 4c - Keypad 5
+    0,          // 4d - Keypad 6
+    0,          // 4e - Keypad "+"
+    0,          // 4f - End / Keypad 1
+    0,          // 50 - Down / Keypad 2
+    0,          // 51 - Page Down / Keypad 3
+    0,          // 52 - Insert / Keypad 0
+    0,          // 53 - Delete / Keypad "."
+    0, 0, 0,    // Nonstandard keys
+    0,          // 57 - F11
+    0,          // 58 - F11
+    0,          // Nonstandard keys
+};
+
+/* Rodney: When CAPS and SHIFT are both pressed, letters are lowercase, but symbols are "shifted" */
+static const uint8_t keyboard_map_shift_and_caps[KEYBOARD_SIZE] = {
+    0,          // 00 - Error code
+    27,         // 01 - Esc
+    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+',
+    '\b',       // 0e - Backspace
+    '\t',       // 0f - Tab
+    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}',
+    '\n',       // 1c - Enter
+    0,          // 1d - Left Control
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', '~',
+    0,          // 2a - Left Shift
+    '|', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?',
+    0,          // 36 - Right Shift
+    '*',        // 37 - Print Screen / Keypad *
+    0,          // 38 - Left Alt
+    ' ',        // 39 - Space
+    0,          // 3a - Caps Lock
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 3b to 44 - F1 to F10
+    0,          // 45 - Num Lock
+    0,          // 46 - Scroll Lock
+    0,          // 47 - Home / Keypad 7
+    0,          // 48 - Up / Keypad 8
+    0,          // 49 - Page Up / Keypad 9
+    '-',        // 4a - Keypad "-"
+    0,          // 4b - Left / Keypad 4
+    0,          // 4c - Keypad 5
+    0,          // 4d - Keypad 6
+    0,          // 4e - Keypad "+"
+    0,          // 4f - End / Keypad 1
+    0,          // 50 - Down / Keypad 2
+    0,          // 51 - Page Down / Keypad 3
+    0,          // 52 - Insert / Keypad 0
+    0,          // 53 - Delete / Keypad "."
+    0, 0, 0,    // Nonstandard keys
+    0,          // 57 - F11
+    0,          // 58 - F11
+    0,          // Nonstandard keys
+};
+
+
 static volatile uint8_t keyboard_state[KEYBOARD_SIZE] = {0};
 static volatile uint8_t caps_lock_status = 0;
 
@@ -261,7 +341,7 @@ void keyboard_handler() {
                 uint8_t alt_pressed = keyboard_state[KEYBOARD_ALT];
 
                 // TODO: handle caps lock differently from shift (symbols should only work with shift, this will require another keymap)
-                uint8_t shift_pressed = keyboard_state[KEYBOARD_LEFT_SHIFT] || keyboard_state[KEYBOARD_RIGHT_SHIFT] || caps_lock_status;
+                uint8_t shift_pressed = keyboard_state[KEYBOARD_LEFT_SHIFT] || keyboard_state[KEYBOARD_RIGHT_SHIFT];;
 
                 uint8_t pressed_char = keyboard_map[(int) keycode];
 
@@ -280,9 +360,17 @@ void keyboard_handler() {
                     pressed_char = 0;
                 }
 
-                // Handle shift
-                if(shift_pressed && pressed_char > 0) {
-                    pressed_char = keyboard_map_shift[(int) keycode];
+                 // Handle Shift, Caps-lock
+                if (pressed_char > 0) {
+                    if (shift_pressed && caps_lock_status) {
+                        pressed_char = keyboard_map_shift_and_caps[(int) keycode];
+                    }
+                    else if (shift_pressed && !caps_lock_status) {
+                        pressed_char = keyboard_map_shift[(int) keycode];
+                    }
+                    else if ( !shift_pressed && caps_lock_status) {
+                        pressed_char = keyboard_map_caps[(int) keycode];
+                    }
                 }
 
                 // Print to screen
